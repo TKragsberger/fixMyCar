c8b2a4fb135fab9d9c122d6016a7840249c9572f
Thomas Kragsberger <thomas_kragsberger@live.dk>
2021-05-17 21:16:20 +0200

    Back ground color change for welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,19 +2,19 @@
 	<div class="h-auto pt-16">
 		<div class="swiper-container mySwiper z-0">
 			<div class="swiper-wrapper">
-				<div class="w-full bg-white overflow-x-hidden flex snap-x swiper-slide" style="height: 80vh">
+				<div class="w-full bg-white overflow-x-hidden flex snap-x swiper-slide" style="height: 70vh">
 					<div class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0 bg-black slides">
 						<img src="https://fgunvs.dk/img/sider/54.jpg" class="h-full w-full object-cover">
 					</div>	
 				</div>
 
-				<div class="w-full bg-white overflow-x-hidden flex snap-x swiper-slide" style="height: 80vh">
+				<div class="w-full bg-white overflow-x-hidden flex snap-x swiper-slide" style="height: 70vh">
 					<div class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0 bg-black">
 						<img src="https://cdn.shortpixel.ai/client/q_glossy,ret_img/https://fgukoldingvejen.dk/wp-content/uploads/FGU_varksted_motor-768x506.jpg" class="h-full w-full object-cover">
 					</div>
 				</div>
 
-				<div class="w-full bg-white overflow-x-hidden flex snap-x swiper-slide" style="height: 80vh">
+				<div class="w-full bg-white overflow-x-hidden flex snap-x swiper-slide" style="height: 70vh">
 					<div class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0 bg-black" id="slide-3">
 						<img src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_768,h_506/https://fgukoldingvejen.dk/wp-content/uploads/2019/03/FGU_fag_motor_mekanik-768x506.jpg" class="h-full w-full object-cover">
 					</div>
@@ -46,72 +46,87 @@
 
 
 
-	<div id="booking"  class="w-full bg-green-700">
+	<div id="booking"  class="w-full bg-gray-300 h-96">
 	  	<br>
-		<div class="text-center text-4xl text-white">Booking</div>
-		<div class="grid grid-cols-3 gap-4 h-80 w-2/4 mx-auto">
-			<div class=" mx-auto my-auto text-center text-white">
+		<div class="text-center text-4xl text-white my-4">Booking</div>
+		<div class="grid grid-cols-3 gap-4 w-2/4 mx-auto">
+			<div class="text-center text-white">
 				<a>
 					<div class="relative">
-						<img class="relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/wheelWhite.png" alt="Workplace" width="600" />
+						<img class="my-2 relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/wheelWhite.png" alt="Workplace" width="600" />
 						
 					</div>
 					<div>Hjul</div>
 				</a>
 			</div>
-			<div class="mx-auto my-auto text-center text-white">
+			<div class="text-center text-white">
 				<a href="{{ route('booking') }}">
 					<div class="relative">
-						<img class="relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/breakWhite.png" alt="Workplace" width="600" />
+						<img class="my-2 relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/breakWhite.png" alt="Workplace" width="600" />
 						
 					</div>
 					<div>Bremser</div>
 				</a>
 			</div>
-			<div class="mx-auto my-auto text-center text-white">
+			<div class="text-center text-white">
 				<a>
 					<div class="relative">
-						<img class="relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/wiperWhite.png" alt="Workplace" width="600" />
+						<img class="my-2 relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/wiperWhite.png" alt="Workplace" width="600" />
 						
 					</div>
 					<div>Vindusvisker</div>
 				</a>
 			</div>
-			<div class="mx-auto my-auto text-center text-white">
+			<div class="text-center text-white">
 				<a>
 					<div class="relative">
-						<img class="relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/lightWhite.png" alt="Workplace" width="600" />
+						<img class="my-2 relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/lightWhite.png" alt="Workplace" width="600" />
 						
 					</div>
 					<div>pære</div>
 				</a>
 			</div>
-			<div class="mx-auto my-auto text-center text-white">
+			<div class="text-center text-white">
 				<a>
 					<div class="relative">
-						<img class="relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/batteryWhite.png" alt="Workplace" width="600" />
+						<img class="my-2 relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/batteryWhite.png" alt="Workplace" width="600" />
 						
 					</div>
 					<div>Batteri</div>
 				</a>	
 			</div>
-			<div class="mx-auto my-auto text-center text-white hover:underline">
+			<div class="text-center text-white hover:underline">
 				<a>
 					<div class="relative">
-						<img class="relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/customWhite.png" alt="Workplace" width="600" />
+						<img class="my-2 relative top-0 left-0 w-16 h-16 mx-auto" src="../resources/images/customWhite.png" alt="Workplace" width="600" />
 						
 					</div>
 					<div>Special anmodning</div>
 				</a>
 			</div>
 		</div>
-		<br>
 	</div>
-	<div id="contact" class="h-96">
+	<div id="Prices" class="h-96">
 			<br>
 			<div class="text-center text-green-700 text-4xl">Priser</div>
-			
-			
+			<br>
+			<div class="grid grid-cols-3 divide-x-4 divide-green-700 w-2/4 mx-auto">
+				<div class="text-center text-green-700">
+					<p class="text-3xl">Hjul skift</p>
+					<p class="text-2xl">Fast pris</p>
+					<p class="text-2xl">250 kr</p>
+				</div>
+				<div class="text-center text-green-700">
+					<p class="text-3xl">Vinduesvisker skift</p>
+					<p class="text-2xl">Fast pris</p>
+					<p class="text-2xl">150 kr</p></div>
+				<div class="text-center text-green-700">
+					<p class="text-3xl">Øverige tjenester</p>
+					<p class="text-2xl">Efter aftale</p>
+					<p class="text-2xl">ca. time pris 150 kr</p>
+				</div>
+			</div>
+			<br>
 			<br>
 			
 		</div>
